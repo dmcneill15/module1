833308953cc29d9e485ce4f38d412759384cd662
dmcneill15 <dvveijeren@gmail.com>
2024-05-11 11:41:29 +1200

    Exercise 4 done, first Node.js

---
+++ Ex4/helloworld.js
@@ -0,0 +1,27 @@
+console.log('Hello World')
+
+function sum(a, b){
+    return(a+b);
+}
+
+function subtract(a, b){
+    return(a-b);
+}
+
+function multiply(a,b){
+    return(a*b);
+}
+
+function divide(a,b){
+    return(a/b);
+}
+
+function greetings(name){
+    return("Hello ".concat(name));
+}
+
+console.log(sum(4, 5));
+console.log(subtract(4, 5));
+console.log(multiply(4, 5));
+console.log(divide(4, 5));
+console.log(greetings("Danielle"));
