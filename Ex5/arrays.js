48da08470187498c8399a1048560f08bccd79f14
dmcneill15 <dvveijeren@gmail.com>
2024-05-11 12:05:08 +1200

    Exercise 5 done, Node.js arrays

---
+++ Ex5/arrays.js
@@ -0,0 +1,13 @@
+let arrayx = ['a', 'b', 'c', 'd', 'e'];
+console.log(arrayx);
+
+arrayx[1] = 'z';
+arrayx[4] = 'y';
+console.log(arrayx);
+
+arrayx.unshift('x');
+console.log(arrayx);
+
+let num1 = arrayx.pop();
+console.log(arrayx);
+console.log("Popped: " + num1);
